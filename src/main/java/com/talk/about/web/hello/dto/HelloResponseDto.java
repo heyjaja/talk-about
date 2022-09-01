b42ca291596f06f21e859c90e1beea294addbd3c
heyjaja <imjaja09@gmail.com>
2022-09-02 01:42:30 +0900

    test, lombok 적용

---
+++ src/main/java/com/talk/about/web/hello/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.talk.about.web.hello.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+
+}
