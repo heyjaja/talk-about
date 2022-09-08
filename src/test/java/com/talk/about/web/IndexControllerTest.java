4c6e9dd3cbfbf2f02cd8e3f9238e7a47e863413c
heyjaja <imjaja09@gmail.com>
2022-09-08 21:22:23 +0900

    머스테치 레이아웃까지

---
+++ src/test/java/com/talk/about/web/IndexControllerTest.java
@@ -0,0 +1,28 @@
+package com.talk.about.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testLoadingMainPage() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링 부트로 게시판 만들기");
+    }
+}
